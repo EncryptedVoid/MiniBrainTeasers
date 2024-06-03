a107c79c61dfd3db5ecc919e94e39a4c1045b39f
AshiqG <Ashiqarib@gmail.com>
2024-06-03 01:18:33 -0400

    OPTIMISED FOR SPEED AND MEMORY

--- CPP-IdentifyingOddAndEvenNumbers.cpp
+++ CPP-IdentifyingOddAndEvenNumbers.cpp
@@ -1,46 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-// Function to check if the input is a valid integer
-int isValidInteger(char *input) {
-    char *endptr;
-    strtol(input, &endptr, 10);
-    return *endptr == '\0';
-}
-
-// Function to check if a number is even or odd
-void checkEvenOdd(int number) {
-    if (number % 2 == 0) {
-        printf("%d is even.\n", number);
-    } else {
-        printf("%d is odd.\n", number);
-    }
-}
 
 int main() {
-    char input[100];
     int number;
     char choice;
 
     do {
         printf("Enter an integer: ");
-        scanf("%s", input);
-
-        // Validate input
-        while (!isValidInteger(input)) {
+        // Use scanf directly to get the integer input
+        while (scanf("%d", &number) != 1) {
+            // Clear the input buffer if the input is not an integer
+            while (getchar() != '\n');
             printf("Invalid input. Please enter a valid integer: ");
-            scanf("%s", input);
         }
 
-        // Convert input to integer
-        number = atoi(input);
-
         // Check if the number is even or odd
-        checkEvenOdd(number);
+        if (number % 2 == 0) {
+            printf("%d is even.\n", number);
+        } else {
+            printf("%d is odd.\n", number);
+        }
 
         // Ask user if they want to continue or exit
         printf("Do you want to check another number? (y/n): ");
         scanf(" %c", &choice);
+        // Clear the input buffer
+        while (getchar() != '\n');
 
     } while (choice == 'y' || choice == 'Y');
 
