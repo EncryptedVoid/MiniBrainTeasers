f13ce3b3f23bc9cee27678343d8cdbff1e6af81e
Ashiq Arib Gazi <Ashiqarib@gmail.com>
2025-01-18 20:29:41 -0500

    TCP Chat Server

---
+++ TCPChatServer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main()
+{
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    listen(server_fd, 3);
+    // Add client handling code
+}
